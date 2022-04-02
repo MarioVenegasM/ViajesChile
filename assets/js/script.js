d4de073a0cba137abe1abebd611b4167bd870fc0
MarioVenegasM <mario.venegas.m@gmail.com>
2022-04-02 17:58:35 -0300

    Agregado toggler a fotos cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,14 @@ $(function() {
         alert("El mensaje fue enviado correctamente...");
     });
 
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    });
 
-
-
+    $('.desaparecer').click(function() {
+        $('.aparece-desaparece').toggle();
+    });
 
 
 
